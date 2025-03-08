c1b3be604e754151282f0a5f3b2e6c4fa8277c3b
sibillagm3wr <sibillagm3wr@gmx.com>
2025-03-08 06:07:57 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,8 @@
+// Random generated JS code for your project
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log("Random number is: " + randomNumber);
+if (randomNumber % 2 === 0) {
+    console.log("The number is even");
+} else {
+    console.log("The number is odd");
+}
